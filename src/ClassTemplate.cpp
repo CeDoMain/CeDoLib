e5ca277d4831440554bcd99200193a683f129939
CeDoMain <cedoms@outlook.de>
2019-04-16 18:46:13 +0200

    * Aufteilung in Ordner wieder rückgängig gemacht

---
+++ src/ClassTemplate.cpp
@@ -0,0 +1,16 @@
+#include "ClassTemplate.h"
+
+ClassTemplate::ClassTemplate()
+{
+
+}
+
+void ClassTemplate::Begin()
+{
+    
+}
+
+void ClassTemplate::Update()
+{
+    
+}
---
+++ src/ClassTemplate.h
@@ -0,0 +1,29 @@
+#ifndef CLASSTEMPLATE_H
+#define CLASSTEMPLATE_H
+
+#include "Arduino.h"
+
+class ClassTemplate
+{
+    // Felder
+public:
+
+private:
+
+    // Konstruktor
+public:
+    ClassTemplate();
+
+    // Methoden
+public:
+    // Initialisiert ClassTemplate
+    void Begin();
+
+    // Muss in jedem Frame aufgerufen werden
+    void Update();
+
+private:
+
+};
+
+#endif
