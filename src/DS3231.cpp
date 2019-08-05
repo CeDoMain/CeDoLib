5810afbbe96a1464f3d3d9e5a89fb3216d3c78f0
CeDoMain <cedoms@outlook.de>
2019-08-05 21:47:00 +0200

    DS3231 + Alarm 1 / 2 + Möglichkeit Sommerzeit nicht zu verwenden + Batterieprüfung

--- src/DS3231.cpp
+++ src/DS3231.cpp
@@ -1,83 +1,241 @@
 #include "DS3231.h"
 
-DS3231::DS3231()
+DS3231::DS3231(bool useSummertime)
+    : Alarm1Event(0), Alarm2Event(0), BatteryLowEvent(0),
+      BatteryCheckTimer(60000), InterruptPin(0), UseSummertime(useSummertime)
 {
-
+    
 }
-
-void DS3231::Begin()
+void DS3231::Begin(DigitalIn* interruptPin)
 {
     Wire.begin();
-}
+    InterruptPin = interruptPin;
 
-void DS3231::SetDateTime(DateTime value)
+    // RTC Konfigurieren
+    byte buffer[2];
+    ReadRegArray(REG_CONTROL, buffer, 2);
+    COPY(buffer[0], 2, 6, B000001);
+    COPY(buffer[1], 0, 4, B0000);
+    WriteRegArray(REG_CONTROL, buffer, 2);
+    
+    BatteryCheckTimer.TimeIsUpEvent = new Delegate<>([&]()
+    {
+        byte data = ReadRegByte(REG_STATUS);
+        if (BatteryLowEvent != 0 && ISSET(data, BIT_OSCILLATOR_STOP_FLAG))
+        {
+            (*BatteryLowEvent)();
+            UNSET(data, BIT_OSCILLATOR_STOP_FLAG);
+            WriteRegByte(REG_STATUS, data);
+        }
+        BatteryCheckTimer.Start();
+    });
+
+    BatteryCheckTimer.Start();
+}
+void DS3231::Update()
+{
+    if (!(*InterruptPin)())
+    {
+        byte data[2];
+        ReadRegArray(REG_CONTROL, data, 2);
+        if (Alarm1Event != 0 && ISSET(data[0], BIT_ALARM1_INT) && ISSET(data[1], BIT_ALARM1_FLAG))
+            (*Alarm1Event)();
+        if (Alarm2Event != 0 && ISSET(data[0], BIT_ALARM2_INT) && ISSET(data[1], BIT_ALARM2_FLAG))
+            (*Alarm2Event)();
+
+        COPY(data[1], 0, 2, B00);
+        WriteRegByte(REG_STATUS, data[1]);
+    }
+    BatteryCheckTimer.Update();
+}
+void DS3231::SetNow(DateTime value)
 {
     // Auf Sommerzeit prüfen
     if (IsSummertime(value))
         value = ToWintertime(value);
 
     // Zeit umwandeln (24 Stunden Format)
-    byte buffer[8] =
+    byte buffer[7] =
     {
-        0x00,
         ToBcd(constrain(value.Second, 0, 59)),
         ToBcd(constrain(value.Minute, 0, 59)),
         ToBcd(constrain(value.Hour, 0, 23)),
-        0x00,
+        constrain(value.DayOfWeek, 1, 7),
         ToBcd(constrain(value.Day, 1, 31)),
         ToBcd(constrain(value.Month, 1, 12)),
-        ToBcd(constrain(value.Year - 2000, 0, 99)),
+        ToBcd(constrain(value.Year - 2000, 0, 99))
     };
     
     // Zeit hochladen
-	Wire.beginTransmission(BASE_ADDRESS);
-    for (byte i = 0; i < sizeof(buffer); i++)
-    {
-        Serial.print(buffer[i], HEX);
-        Serial.print(' ');
-        Wire.write(buffer[i]);
-    }
-    Serial.println();
-	Wire.endTransmission();
+    WriteRegArray(REG7_NOW, buffer, 7);
 }
-
-DS3231::DateTime DS3231::GetDateTime()
+DS3231::DateTime DS3231::GetNow()
 {
     // Zeit auslesen
     byte buffer[7];
-	Wire.beginTransmission(BASE_ADDRESS);
-	Wire.write(0x00);
-	Wire.endTransmission();
-	Wire.requestFrom(BASE_ADDRESS, 7);
-    for (byte i = 0; i < sizeof(buffer); i++)
-        buffer[i] = Wire.read();
+    ReadRegArray(REG7_NOW, buffer, 7);
 
-    // Zeit umwandeln
+    // Zeit umwandeln (24 Stunden Format)
     DateTime now;
     now.Second = FromBcd(buffer[0]);
     now.Minute = FromBcd(buffer[1]);
-    now.Hour = FromBcd(EXTRACT(buffer[2], 0, 5));
-    // 12/24 Stunden verarbeiten
-    if (ISSET(buffer[2], 5))
-        now.Hour += ISSET(buffer[2], 6) ? 12 : 20;
+    now.Hour = FromBcd(EXTRACT(buffer[2], 0, 6));
+    now.DayOfWeek = buffer[3];
     now.Day = FromBcd(buffer[4]);
     now.Month = FromBcd(EXTRACT(buffer[5], 0, 5));
     now.Year = 2000 + FromBcd(buffer[6]);
 
     return IsSummertime(now) ? ToSummertime(now) : now;
 }
+void DS3231::SetAlarm1(DateTime value, Alarm1Rate rate)
+{
+    // Auf Sommerzeit prüfen
+    if (IsSummertime(value))
+        value = ToWintertime(value);
 
+    // Zeit umwandeln (24 Stunden Format)
+    byte buffer[4] =
+    {
+        ToBcd(constrain(value.Second, 0, 59)),
+        ToBcd(constrain(value.Minute, 0, 59)),
+        ToBcd(constrain(value.Hour, 0, 23)),
+        rate == Alarm1Rate::MatchDayHMS
+            ? constrain(value.DayOfWeek, 1, 7) + 0x40
+            : ToBcd(constrain(value.Day, 1, 31))
+    };
+
+    // Flags setzen
+    for (int i = 0; i < 4; i++)
+    {
+        if (ISSET(rate, i))
+            SET(buffer[i], 7);
+    }
+    
+    // Konfiguration hochladen
+    WriteRegArray(REG4_ALARM1, buffer, 4);
+}
+void DS3231::SetAlarm2(DateTime value, Alarm2Rate rate)
+{
+    // Auf Sommerzeit prüfen
+    if (IsSummertime(value))
+        value = ToWintertime(value);
+
+    // Zeit umwandeln (24 Stunden Format)
+    byte buffer[3] =
+    {
+        ToBcd(constrain(value.Minute, 0, 59)),
+        ToBcd(constrain(value.Hour, 0, 23)),
+        rate == Alarm2Rate::MatchDayHM
+            ? constrain(value.DayOfWeek, 1, 7) + 0x40
+            : ToBcd(constrain(value.Day, 1, 31))
+    };
+
+    // Flags setzen
+    for (int i = 0; i < 3; i++)
+    {
+        if (ISSET(rate, i))
+            SET(buffer[i], 7);
+    }
+    
+    // Konfiguration hochladen
+    WriteRegArray(REG3_ALARM2, buffer, 3);
+}
+void DS3231::SetEnableAlarm1(bool isEnabled)
+{
+    byte data = ReadRegByte(REG_CONTROL);
+    if (isEnabled != ISSET(data, BIT_ALARM1_INT))
+    {
+        isEnabled ? SET(data, BIT_ALARM1_INT) : UNSET(data, BIT_ALARM1_INT);
+        WriteRegByte(REG_CONTROL, data);
+    }
+}
+void DS3231::SetEnableAlarm2(bool isEnabled)
+{
+    byte data = ReadRegByte(REG_CONTROL);
+    if (isEnabled != ISSET(data, BIT_ALARM2_INT))
+    {
+        isEnabled ? SET(data, BIT_ALARM2_INT) : UNSET(data, BIT_ALARM2_INT);
+        WriteRegByte(REG_CONTROL, data);
+    }
+}
+void DS3231::GetAlarm1(DateTime* value, Alarm1Rate* rate, bool* isEnabled)
+{
+    // Daten auslesen
+    byte buffer[6];
+    ReadRegArray(REG4_ALARM1, buffer, 4);
+    buffer[4] = ReadRegByte(REG_CONTROL);
+    buffer[5] = ReadRegByte(REG_STATUS);
+
+    if (value != 0)
+    {
+        // Zeit umwandeln (24 Stunden Format)
+        DateTime time;
+        time.Second = FromBcd(EXTRACT(buffer[0], 0, 7));
+        time.Minute = FromBcd(EXTRACT(buffer[1], 0, 7));
+        time.Hour = FromBcd(EXTRACT(buffer[2], 0, 6));
+        (ISSET(buffer[3], 6) ? time.DayOfWeek : time.Day) = FromBcd(EXTRACT(buffer[3], 0, 6));
+        *value = IsSummertime(time) ? ToSummertime(time) : time;
+    }
+    if (rate != 0)
+    {
+        // Flags prüfen
+        byte rawRate = 0x00;
+        for (int i = 0; i < 4; i++)
+        {
+            if (ISSET(buffer[i], 7))
+                SET(rawRate, i);
+        }
+        if (ISSET(buffer[3], 6))
+            SET(rawRate, 4);
+        *rate = (Alarm1Rate)rawRate;
+    }
+    if (isEnabled != 0)
+        *isEnabled = ISSET(buffer[4], BIT_ALARM1_INT);
+}
+void DS3231::GetAlarm2(DateTime* value, Alarm2Rate* rate, bool* isEnabled)
+{
+    // Daten auslesen
+    byte buffer[4];
+    ReadRegArray(REG3_ALARM2, buffer, 4);
+
+    if (value != 0)
+    {
+        // Zeit umwandeln (24 Stunden Format)
+        DateTime time;
+        time.Minute = FromBcd(EXTRACT(buffer[0], 0, 7));
+        time.Hour = FromBcd(EXTRACT(buffer[1], 0, 6));
+        (ISSET(buffer[2], 6) ? time.DayOfWeek : time.Day) = FromBcd(EXTRACT(buffer[2], 0, 6));
+        *value = IsSummertime(time) ? ToSummertime(time) : time;
+    }
+    if (rate != 0)
+    {
+        // Flags prüfen
+        byte rawRate = 0x00;
+        for (int i = 0; i < 3; i++)
+        {
+            if (ISSET(buffer[i], 7))
+                SET(rawRate, i);
+        }
+        if (ISSET(buffer[2], 6))
+            SET(rawRate, 3);
+        *rate = (Alarm2Rate)rawRate;
+    }
+    if (isEnabled != 0)
+        *isEnabled = ISSET(buffer[3], BIT_ALARM2_INT);
+}
 bool DS3231::IsSummertime(DateTime rawValue)
 {
+    if (!UseSummertime)
+        return false;
+        
     byte lastSundayOfMar = 31 - (5 + rawValue.Year + rawValue.Year / 4) % 7;
     byte lastSundayOfOct = 31 - (2 + rawValue.Year + rawValue.Year / 4) % 7;
     return (rawValue.Month > 3 && rawValue.Month < 10)                                          // zwischen März und Oktober
         || (rawValue.Month == 3 && rawValue.Day > lastSundayOfMar)                              // oder nach dem letzten Sonntag im März
         || (rawValue.Month == 10 && rawValue.Day < lastSundayOfOct)                             // oder vor dem letzten Sonntag im Oktober
-        || (rawValue.Month == 3 && rawValue.Day == lastSundayOfMar && rawValue.Hour >= 2)                // oder am letzten Sonntag im März nach 2:00
+        || (rawValue.Month == 3 && rawValue.Day == lastSundayOfMar && rawValue.Hour >= 2)       // oder am letzten Sonntag im März nach 2:00
         || (rawValue.Month == 10 && rawValue.Day == lastSundayOfOct && rawValue.Hour < 2);      // oder am letzten Sonntag im Oktober vor 2:00
 }
-
 DS3231::DateTime DS3231::ToSummertime(DateTime value)
 {
     // +1 Stunde
@@ -87,6 +245,7 @@ DS3231::DateTime DS3231::ToSummertime(DateTime value)
         // nächster Tag
         value.Hour = 0;
         value.Day++;
+        value.DayOfWeek += value.DayOfWeek == 7 ? -6 : 1;
     }
     // verschiedene Monatslängen zwischen März und Oktober beachten
     switch (value.Month)
@@ -110,7 +269,6 @@ DS3231::DateTime DS3231::ToSummertime(DateTime value)
     }
     return value;
 }
-
 DS3231::DateTime DS3231::ToWintertime(DateTime value)
 {
     // -1 Stunde
@@ -119,6 +277,7 @@ DS3231::DateTime DS3231::ToWintertime(DateTime value)
         // vorheriger Tag
         value.Hour = 23;
         value.Day--;
+        value.DayOfWeek -= value.DayOfWeek == 1 ? -6 : 1;
     }
     else
     {
@@ -142,13 +301,55 @@ DS3231::DateTime DS3231::ToWintertime(DateTime value)
     }
     return value;
 }
-
 byte DS3231::FromBcd(byte value)
 {
     return EXTRACT(value, 4, 4) * 10 + EXTRACT(value, 0, 4);
 }
-
 byte DS3231::ToBcd(byte value)
 {
     return ((value / 10) << 4) + (value % 10);
+}
+void DS3231::WriteRegByte(byte regAddress, byte data)
+{
+	// 1 Byte in angegebenes Register schreiben
+	Wire.beginTransmission(BASE_ADDRESS);
+	Wire.write(regAddress);
+	Wire.write(data);
+	Wire.endTransmission();
+}
+void DS3231::WriteRegArray(byte regAddress, byte* data, byte count)
+{
+	// Mehrere Bytes in angegebenes Register schreiben
+	Wire.beginTransmission(BASE_ADDRESS);
+	Wire.write(regAddress);
+    for (int i = 0; i < count; i++)
+    {
+        Wire.write(data[i]);
+    }
+	Wire.endTransmission();
+}
+byte DS3231::ReadRegByte(byte regAddress)
+{
+	// Byte aus angegebenem Register anfordern
+	Wire.beginTransmission(BASE_ADDRESS);
+	Wire.write(regAddress);
+	Wire.endTransmission(false);
+	Wire.requestFrom((int)BASE_ADDRESS, (int)1);
+
+	// Byte einlesen
+    return Wire.available() ? Wire.read() : 0x00;
+}
+void DS3231::ReadRegArray(byte regAddress, byte* data, byte count)
+{
+	// Mehrere Bytes aus angegebenem Register anfordern
+	Wire.beginTransmission(BASE_ADDRESS);
+	Wire.write(regAddress);
+	Wire.endTransmission(false);
+	Wire.requestFrom((int)BASE_ADDRESS, (int)count);
+
+	// Bytes einlesen
+    for (int i = 0; i < count; i++)
+	{
+        data[i] = Wire.available() ? Wire.read() : 0x00;
+	}
 }
--- src/DS3231.h
+++ src/DS3231.h
@@ -4,6 +4,7 @@
 #include "Arduino.h"
 #include "Wire.h"
 #include "BaseIO.h"
+#include "Timer.h"
 
 class DS3231
 {
@@ -13,38 +14,103 @@ public:
     {
         unsigned short Year;
         byte Month, Day, Hour, Minute, Second;
+        byte DayOfWeek;     // Von 1 bis 7
+        
+        DateTime() : Year(2000), Month(1), Day(1), Hour(0), Minute(0), Second(0), DayOfWeek(6) { }
+        bool operator!= (DateTime t)
+        {
+            return Second != t.Second || Minute != t.Minute || Hour != t.Hour
+                || Day != t.Day || Month != t.Month || Year != t.Year;
+        }
+        bool operator== (DateTime t)
+        {
+            return Second == t.Second && Minute == t.Minute && Hour == t.Hour
+                && Day == t.Day && Month == t.Month && Year == t.Year;
+        }
+    };
+    enum Alarm1Rate
+    {
+        OncePerS =      B01111,
+        MatchS =        B01110,
+        MatchMS =       B01100,
+        MatchHMS =      B01000,
+        MatchDateHMS =  B00000,
+        MatchDayHMS =   B10000
+    };
+    enum Alarm2Rate
+    {
+        OncePerM =      B0111,
+        MatchM =        B0110,
+        MatchHM =       B0100,
+        MatchDateHM =   B0000,
+        MatchDayHM =    B1000
     };
     
     // Felder
+public:
+    Delegate<>* Alarm1Event;
+    Delegate<>* Alarm2Event;
+    Delegate<>* BatteryLowEvent;
+
 private:
-    static const byte BASE_ADDRESS = 0x68;      // Basisadresse des Chips
+    static const byte BASE_ADDRESS = 0x68;
+    static const byte REG7_NOW = 0x00;
+    static const byte REG4_ALARM1 = 0x07;
+    static const byte REG3_ALARM2 = 0x0B;
+    static const byte REG_CONTROL = 0x0E;
+    static const byte BIT_ALARM2_INT = 1;
+    static const byte BIT_ALARM1_INT = 0;
+    static const byte REG_STATUS = 0x0F;
+    static const byte BIT_OSCILLATOR_STOP_FLAG = 7;
+    static const byte BIT_ALARM2_FLAG = 1;
+    static const byte BIT_ALARM1_FLAG = 0;
+
+    Timer BatteryCheckTimer;
+    DigitalIn* InterruptPin;
+
+    const bool UseSummertime;
 
     // Konstruktor
 public:
-    DS3231();
+    DS3231(bool useSummertime = true);
 
     // Methoden
 public:
     // Initialisiert den Chip
-    void Begin();
+    void Begin(DigitalIn* interruptPin);
 
-    // Ändert die aktuelle Uhrzeit und Datum
-    void SetDateTime(DateTime value);
+    // Muss in jedem Frame aufgerufen werden
+    void Update();
 
-    // Ließt die aktuelle Uhrzeit und Datum
-    DateTime GetDateTime();
+    // Uhrzeit und Datum
+    void SetNow(DateTime value);
+    DateTime GetNow();
+
+    // Alarme
+    void SetAlarm1(DateTime value, Alarm1Rate rate);
+    void SetAlarm2(DateTime value, Alarm2Rate rate);
+    void SetEnableAlarm1(bool isEnabled);
+    void SetEnableAlarm2(bool isEnabled);
+    void GetAlarm1(DateTime* value, Alarm1Rate* rate = 0, bool* isEnabled = 0);
+    void GetAlarm2(DateTime* value, Alarm2Rate* rate = 0, bool* isEnabled = 0);
 
 private:
     // Gibt an ob die angegebene Normalzeit in der Sommerzeit liegt
     bool IsSummertime(DateTime rawValue);
 
-    // Wird intern verwendet um Sommer- und Winterzeit umzurechnen
+    // Rechnet Sommer- und Winterzeit um
     DateTime ToSummertime(DateTime value);
     DateTime ToWintertime(DateTime value);
 
-    // Wird intern verwendet um BCD Zahlen umzuwandeln
+    // Wandelt BCD Zahlen um
     byte FromBcd(byte value);
     byte ToBcd(byte value);
+
+    // Daten senden und empfangen
+    void WriteRegByte(byte regAddress, byte data);
+    void WriteRegArray(byte regAddress, byte* data, byte count);
+    byte ReadRegByte(byte regAddress);
+    void ReadRegArray(byte regAddress, byte* data, byte count);
 };
 
 #endif
